c1faab1ce0baf16260ec1e5f305481d24e49d33d
naoto-yoda <mac_prog0128@icloud.com>
2022-05-31 11:59:54 +0900

    PHPで作成したお問い合わせフォーム

---
+++ contactform/insert.php
@@ -0,0 +1,39 @@
+<?php
+$dsn = 'mysql:dbname=lesson01;host=localhost';
+$user = 'root';
+$password = 'mysql';
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+mb_internal_encoding("utf8");
+
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;", "root", "mysql");
+
+$pdo->exec("insert into contactform(name, mail, age, comments)
+values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');");
+
+?>
+
+<!doctype HTML>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>お問い合わせフォーム</title>
+        <link rel="stylesheet" type="text/css" href="style2.css">
+    </head>
+
+    <body>
+        <h1>お問い合わせフォーム</h1>
+
+        <div class="confirm">
+            <p>お問い合わせ有難うございました。<br>
+                3営業日以内に担当者よりご連絡差し上げます。
+            </p>
+        </div><!-- .confirm -->
+    </body>
+</html>
